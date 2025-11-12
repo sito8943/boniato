af50015ccc79d141afc0d775d4e3979f04da4402
Nico Deblauwe <nico@bluepundit.eu>
2025-11-12 15:44:09 +0100

    DESIGN :art: Alternative text if no comments

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,11 +5,14 @@
     {{$article->content}}
 
     <h2 class="font-bold mb-2 mt-8">Comments</h2>
-    <ul class="list-disc pl-4">
-
-    @foreach($article->comments as $comment)
-        <li>{{$comment->content}}</li>
-    @endforeach
-    </ul>
+    @if($article->comments->isNotEmpty())
+        <ul class="list-disc pl-4">
+            @foreach($article->comments as $comment)
+                <li>{{$comment->content}}</li>
+            @endforeach
+        </ul>
+    @else
+        No comments yet
+    @endif
 
 </x-site-layout>
