81e7c4fd585d3ca24623cb23a6636f3420e86d3a
Nico Deblauwe <nico@bluepundit.eu>
2025-11-14 17:11:56 +0100

    DESIGN :art: Improve formtext layout

--- resources/views/components/form-text.blade.php
+++ resources/views/components/form-text.blade.php
@@ -1,11 +1,13 @@
-<label>{{$label}}:</label>
-<input
-    type="text"
-    name="{{$name}}"
-    placeholder="{{$placeholder}}"
-    class="border @error($name) border-red-500 @else border-black @enderror"
->
-@error($name)
-<div class="text-red-500">{{$message}}</div>
-@enderror
+<div class="mb-4">
+    <label>{{$label}}:</label><br/>
+    <input
+        type="text"
+        name="{{$name}}"
+        placeholder="{{$placeholder}}"
         value="{{old($name,$value)}}"
+        class="w-full border @error($name) border-red-500 @else border-black @enderror"
+    >
+    @error($name)
+    <div class="text-red-500">{{$message}}</div>
+    @enderror
+</div>
