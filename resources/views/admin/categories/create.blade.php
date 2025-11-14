b5869fc5585d0d07929b5507ad23caf8335c5b6f
Nico Deblauwe <nico@bluepundit.eu>
2025-11-14 15:26:20 +0100

    REFACTOR :hammer: Start using our formtext component

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -3,17 +3,8 @@
     <form action="/admin/categories" method="post">
         @csrf
 
-        <label>Category:</label>
-        <input
-            type="text"
-            name="name"
-            placeholder="Add a category name"
-            value="{{old('name')}}"
-            class="border @error('name') border-red-500 @else border-black @enderror"
-        >
-        @error('name')
-        <div class="text-red-500">{{$message}}</div>
-        @enderror
+        <x-form-text name="name" label="Category" placeholder="Add a category name" />
+
 
 
         <div class="mt-4">
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -4,17 +4,7 @@
         @csrf
         @method('PUT')
 
-        <label>Category:</label>
-        <input
-            type="text"
-            name="name"
-            placeholder="Add a category name"
-            value="{{old('name',$category->name)}}"
-            class="border @error('name') border-red-500 @else border-black @enderror"
-        >
-        @error('name')
-            <div class="text-red-500">{{$message}}</div>
-        @enderror
+        <x-form-text name="name" label="Category" placeholder="Add a category name" value="{{$category->name}}" />
 
         <div class="mt-4">
             <button class="bg-gray-200 p-2" type="submit">Update</button>
