9725961bd9cf39ecae998f704a07ab704756a28e
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-24 15:08:05 +0100

    FIX: Categories management when creating an article

--- app/Http/Controllers/AdminArticleController.php
+++ app/Http/Controllers/AdminArticleController.php
@@ -12,9 +12,9 @@ class AdminArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::where('author_id', auth()->id() )->get();
+        $articles = Article::where('author_id', auth()->id())->get();
 
-        if(auth()->user()->is_admin) {
+        if (auth()->user()->is_admin) {
             $articles = Article::all();
         }
 
@@ -39,7 +39,14 @@ class AdminArticleController extends Controller
             'content' => ['nullable', 'string', 'min:10', 'max:500'],
         ]);
 
-        Article::create($validated + ['author_id' => 1]);
+        $article = Article::create($validated + ['author_id' => 1]);
+
+        if ($request->has('categories')) {
+            $article->categories()->sync($validated['categories']);
+            unset($validated['categories']);
+        } else {
+            $article->categories()->sync([]);
+        }
 
         return redirect('/admin/articles');
     }
@@ -61,7 +68,7 @@ class AdminArticleController extends Controller
     {
         $article = Article::find($id);
 
-        if(!$article->canBeManagedBy(auth()->user())) {
+        if (!$article->canBeManagedBy(auth()->user())) {
             abort(403);
         }
 
@@ -75,7 +82,7 @@ class AdminArticleController extends Controller
     {
         $article = Article::find($id);
 
-        if(!$article->canBeManagedBy(auth()->user())) {
+        if (!$article->canBeManagedBy(auth()->user())) {
             abort(403);
         }
 
@@ -87,7 +94,7 @@ class AdminArticleController extends Controller
             'author_id' => ['nullable', 'integer', 'exists:users,id'],
         ]);
 
-        if($request->has('categories')) {
+        if ($request->has('categories')) {
             $article->categories()->sync($validated['categories']);
             unset($validated['categories']);
         } else {
@@ -95,7 +102,7 @@ class AdminArticleController extends Controller
         }
 
         // First process the file and upload it and get reference
-        if($request->hasFile('photo')) {
+        if ($request->hasFile('photo')) {
             $article->media->each->delete();
             $article->addMediaFromRequest('photo')->toMediaCollection();
             unset($validated['photo']);
@@ -114,7 +121,7 @@ class AdminArticleController extends Controller
     {
         $article = Article::find($id);
 
-        if(!$article->canBeManagedBy(auth()->user())) {
+        if (!$article->canBeManagedBy(auth()->user())) {
             abort(403);
         }
 
